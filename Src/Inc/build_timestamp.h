e8a86633da49ba15cda4a6ee65416f11d5651da7
maker99 <maker99@noreply.github.com>
2020-06-21 19:00:55 +0200

    fixing missing build action

--- Src/Inc/build_timestamp.h
+++ Src/Inc/build_timestamp.h
@@ -1,5 +1,6 @@
 #ifndef BUILD_TIMESTAMP
-#define HGREV "9002fbe1"
+#warning Mercurial failed. Repository not found. Firmware revision will not be generated. 
+#define HGREV N/A 
 #define BUILD_TIMESTAMP "2020-06-15 17:49 UT"
 #define HGREVSTR(s) stringify_(s)
 #define stringify_(s) #s
